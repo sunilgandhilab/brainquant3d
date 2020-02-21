09923b2ba1e014c504115f52259a411b1a9142e2
blakjak44 <blakjak44@gmail.com>
2020-02-20 16:13:07 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -212,9 +212,15 @@ class install(_install):
 
         # install antspy
         if sys.platform == 'linux':
-            main(['install', "https://github.com/ANTsX/ANTsPy/releases/download/v0.1.4/antspy-0.1.4-cp36-cp36m-linux_x86_64.whl"])
+            try:
+                main(['install', '--user', "https://github.com/ANTsX/ANTsPy/releases/download/v0.1.4/antspy-0.1.4-cp36-cp36m-linux_x86_64.whl"])
+            except:
+                main(['install', '--user', "https://github.com/ANTsX/ANTsPy/releases/download/v0.2.0/antspyx-0.2.0-cp37-cp37m-linux_x86_64.whl"])
         if sys.platform == 'darwin':
-            main(['install', "https://github.com/ANTsX/ANTsPy/releases/download/Weekly/antspy-0.1.4-cp36-cp36m-macosx_10_7_x86_64.whl"])
+            try:
+                main(['install', '--user', "https://github.com/ANTsX/ANTsPy/releases/download/Weekly/antspy-0.1.4-cp36-cp36m-macosx_10_7_x86_64.whl"])
+            except:
+                main(['install', '--user', "https://github.com/ANTsX/ANTsPy/releases/download/v0.2.0/antspyx-0.2.2-cp37-cp37m-macosx_10_15_x86_64.whl"])
 
 
 cmdclass['install'] = install
