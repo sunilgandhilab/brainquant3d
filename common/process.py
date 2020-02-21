1dedefe113ad985bf08d66d4f7b414ff2453b694
blakjak44 <blakjak44@gmail.com>
2020-02-21 11:21:51 -0800

    Update process.py

--- common/process.py
+++ common/process.py
@@ -3,35 +3,38 @@
 script to run the processing pipeline
 """
 
-#load the parameters:
+# Load the parameters:
 exec(open("/mnt/e/jack-temp/brainquant3d-tutorial/parameter.py").read())
 
-#Cell detection:
+################
+## Cell Detection:
 ################
 from bq3d.stack_processing.cell_detection import detect_cells
 detect_cells(**CellDetectionParams);
 
-#######
-## resampling operations:
-#######
-#resampling for the correction of stage movements during the acquisition between channels:
+################
+## Resampling:
+################
+# Downsampling for the correction of stage movements during the acquisition between channels:
 from bq3d.alignment.resampling import resampleData;
 resampleData(**CorrectionResamplingParamSignal);
 resampleData(**CorrectionResamplingParamAuto);
-#Downsampling for alignment to the Atlas:
+# Downsampling for alignment to the Atlas:
 resampleData(**RegistrationResamplingParamSignal);
 resampleData(**RegistrationResamplingParamAuto);
 
-#Alignment operations:
-#####################
-from bq3d.alignment.ants import alignData, transformPoints, transformImage
+################
+## Alignment:
+################
+from bq3d.alignment.ants import alignData
 #correction between channels:
 alignData(**CorrectionAlignmentParam)
 #alignment to the Atlas:
 alignData(**RegistrationAlignmentParam)
 
-# Transform point coordinates
-############################
+################
+## Transform Point Coordinates:
+################
 from bq3d import io
 from bq3d.alignment.resampling import resamplePoints
 from bq3d.alignment.ants import transformPoints
@@ -39,27 +42,27 @@ from bq3d.analysis.voxelization import voxelize
 from bq3d.stack_processing.cell_detection import jsonify_points
 
 points = io.readPoints(sink)
-# downsample to chromatic correction size
+# Downsample to chromatic correction size
 points = resamplePoints(sink, **CorrectionResamplingPointsParam);
 vox = voxelize(points, CorrectionResamplingParamSignal['sink'] , sink = os.path.join(BaseDirectory, 'cells_cd1.tif'), **voxelizeParameter);
-# apply correction transform
+# Apply correction transform
 points = transformPoints(points, CorrectionAlignmentParam["resultDirectory"], invert = True);
 vox = voxelize(points, CorrectionResamplingParamSignal['sink'] , sink = os.path.join(BaseDirectory, 'cells_corr.tif'), **voxelizeParameter);
-# upsample back to original size
+# Upsample back to original size
 points = resamplePoints(points, **CorrectionResamplingPointsInverseParam);
 #vox = voxelize(points, SignalFile , sink = os.path.join(BaseDirectory, 'cells_full.tif'), **voxelizeParameter);
-# downsample to atlas resolution
+# Downsample to atlas resolution
 points = resamplePoints(points, **RegistrationResamplingPointParam);
 vox = voxelize(points, RegistrationResamplingParamAuto['sink'], sink = os.path.join(BaseDirectory, 'cells_ds.tif'), **voxelizeParameter);
-# warp to atlas
+# Warp to atlas
 points = transformPoints(points, RegistrationAlignmentParam["resultDirectory"], invert = True);
-# write out heatmap and transformed points
+# Write out heatmap and transformed points
 vox = voxelize(points, AtlasFile, sink = os.path.join(BaseDirectory, 'cells_atlas.tif'), **voxelizeParameter);
 points = points.T.tolist()
 points_with_props = {**io.readData(sink), 'z': points[0], 'y': points[1], 'x': points[2]}
 io.writePoints(transformedCellsFile, points_with_props);
 
-# regional analysis
+# Regional analysis
 from bq3d.analysis.region_atlas import Atlas
 print('generating atlas')
 a = Atlas(collapse=True)
