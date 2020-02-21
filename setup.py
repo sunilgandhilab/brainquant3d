b3874bd3542428e65c4d0d55cf120d7f075ff6e0
blakjak44 <blakjak44@gmail.com>
2020-02-20 17:23:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ class install(_install):
 
         #  download external programs required by package to install directory.
         cwd = os.getcwd()
-        build_dir = glob(os.path.join(cwd, 'build/lib.linux*'))[0]
+        build_dir = glob(os.path.join(cwd, 'build/lib.*'))[0]
         dest = Path(build_dir) / 'bq3d/.external'
 
         print('installing elastik')
